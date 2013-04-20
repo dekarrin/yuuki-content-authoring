b0f0266c0adb3bd856a16a3dabb90df8f71e5c34
TF Nelson <tails.fanatic@gmail.com>
2013-04-20 15:20:16 -0500

    Added index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php require "includes/common.php" ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<title>YCAT :: Home</title>
+	</head>
+	<body>
+		<p>Welcome to authoring tools. Build something creative by utilizing your thinking brain-machine.</p>
+	</body>
+</html>
