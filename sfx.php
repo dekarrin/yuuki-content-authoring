c557b924e437b2e5ea0d6bb90b3738809736fd3c
Brady <brady.p.hanson@gmail.com>
2013-04-22 00:13:43 -0500

    Cleaned up sfx.php added uploading funcitonality

--- sfx.php
+++ sfx.php
@@ -2,22 +2,27 @@
 <body>
 
 	<h1>Sound Effects</h1>
-	<?php>
-		//$results = mysql_query("SELECT * FROM SoundEffect");
-		//$numrows = mysql_numrows($results);
-		echo "<table>";
-		echo "<tr><td>Index</td> <td>Filename</td> <tr>"
-		for($i = 0; $i < $numrows; i++){
-			//$index = mysql_result($result, $i, "index");
-			//$filename = mysql_result($result, $i, "filename");
-			
-			echo "<tr>\n";
-			echo "<td>$id</td> <td>$filename</td>\n";
-			echo "<tr>\n";
-		}
-		echo "</table">";
-
-	</?>
+	<table border = 1>
+		<tr><td>Index</td><td>File Name</td></tr>
+		<?php
+			//$result = mysql_query("SELECT * FROM SoundEffect);
+			//$numrows = mysql_numrows(%result);
+			//for($i = 0; $i < $numberrows; i++){
+				//$index = mysql_result($result, $i, "index");
+				//$filename = mysql_result($result, $i, "filename");
+				//echo "<tr>";
+				//echo "<td>$id</td> <td>$filename</td>";
+				//echo "</tr>";
+			//}
+		?>
+	</table>
+	
+	<!--TODO: upload_file.php needds to be coded-->
+	<form action = "upload_file.php" method = "post" enctype = "multipart/form-data">
+	<label for = "file">Filename:</label>
+	<input type = "file" name = "file" id = "file"<br>
+	<input type = "submit" name = "submit" value = "Submit">
+	</form>
 
 </body>
 </html>
