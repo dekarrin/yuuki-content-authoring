0148acd8463a8305d36de04fe37cae164d563049
Brady <brady.p.hanson@gmail.com>
2013-04-29 13:56:39 -0500

    Fixed display error in entity.php

--- entity.php
+++ entity.php
@@ -13,8 +13,8 @@
 			foreach($usable_entities as $u){
 				echo "<tr>";
 				echo "<td>{$u['id']}</td><td>{$u['name']}</td><td>{$u['sprite']}</td><td>{$u['cid']}</td>
-				{$u['hpBase']}<td>{$u['hpGain']}</td><td>{$u['mpBase']}</td><td>{$u['mpGain']}</td>
-				<td>{$u['strengthBase']}</td><td></td><td>{$u['strengthGain']}</td><td>{$u['defenseBase']}</td><td>{$u['defenseGain']}</td>
+				<td>{$u['hpBase']}</td><td>{$u['hpGain']}</td><td>{$u['mpBase']}</td><td>{$u['mpGain']}</td>
+				<td>{$u['strengthBase']}</td><td>{$u['strengthGain']}</td><td>{$u['defenseBase']}</td><td>{$u['defenseGain']}</td>
 				<td>{$u['agilityBase']}</td><td>{$u['agilityGain']}</td><td>{$u['accuracyBase']}</td><td>{$u['accuracyGain']}</td>
 				<td>{$u['magicBase']}</td><td>{$u['magicGain']}</td><td>{$u['luckBase']}</td><td>{$u['luckGain']}</td><td>{$u['xp']}</td>";
 				echo "</tr>";
@@ -28,7 +28,7 @@
 		MP Gain: <input type="text" name="mpgain"><br>
 		Strength Base: <input type="text" name="strengthbase"><br>
 		Strength Gain: <input type="text" name="strengthgain"><br>
-		Defense Base: <input type="text" name="defensebase"><br>La
+		Defense Base: <input type="text" name="defensebase"><br>
 		Defense Gain: <input type="text" name="defensegain"><br>
 		Agility Base: <input type="text" name="agilitybase"><br>
 		Agility Gain: <input type="text" name="agilitygain"><br>
